45d44331136ccd7dfedfb2069420e7648fce7733
kizzzil <kizzzil5@gmail.com>
2025-05-12 16:44:28 +0300

    init gradle project

---
+++ src/main/kotlin/com/example/demo/HackPracticeApplication.kt
@@ -0,0 +1,11 @@
+package com.example.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HackPracticeApplication
+
+fun main(args: Array<String>) {
+	runApplication<HackPracticeApplication>(*args)
+}
