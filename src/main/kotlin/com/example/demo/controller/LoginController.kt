32365b6c589a1b4a9545787d6134056bd8ea0e6f
kizzzil <kizzzil5@gmail.com>
2025-05-20 09:52:22 +0300

    add login controller

---
+++ src/main/kotlin/com/example/demo/controller/LoginController.kt
@@ -0,0 +1,11 @@
+package com.example.demo.controller
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class LoginController {
+    @GetMapping("/login")
+    fun home(): String = "Coming soon..\nThere will be a login page here"
+}
+
